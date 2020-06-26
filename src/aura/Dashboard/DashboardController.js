b5c6b81bfb4628d9c91096fb130f64c641259963
Roman <romabukhtiarov@gmail.com>
2020-06-26 07:54:02 +0300

    Impl display account name and account number

--- src/aura/Dashboard/DashboardController.js
+++ src/aura/Dashboard/DashboardController.js
@@ -1,7 +1,41 @@
 ({
+  onInit: function (component, event, helper) {
+    var action = component.get("c.getUserID");
+    action.setCallback(this, function(response) {
+    var state = response.getState();
+    if(state === "SUCCESS") {
+    var result = response.getReturnValue();
+    result = 'Account number: ' + result;
+    component.set('v.userID', result);
+    }
+    else {
+    component.set('v.userID', 'fail');
+    }
+    });
+      $A.enqueueAction(action);
+  },
+
+  onInit2: function (component, event, helper) {
+    var action = component.get("c.getUserName");
+    action.setCallback(this, function(response) {
+    var state = response.getState();
+    if(state === "SUCCESS") {
+    var result = response.getReturnValue();
+    result = ' Account name: ' + result;
+    component.set('v.userName', result);
+    }
+    else {
+    component.set('v.userName', 'fail');
+    }
+    });
+      $A.enqueueAction(action);
+  },
+
   createProduct: function (component, event, helper) {
 
-  }
+  },
+
+
 
   openCart: function (component, event, helper) {
 
