4adc14b15522b66b5d5f1036b485a44bfc361678
Roman <romabukhtiarov@gmail.com>
2020-06-30 22:15:24 +0300

    Impl product search

--- src/aura/Product/ProductController.js
+++ src/aura/Product/ProductController.js
@@ -41,5 +41,21 @@
                       });
                       action.setParams({  index : indexOfProduct  });
                       $A.enqueueAction(action);
+    },
+    searchProducts: function (component, event, helper) {
+     var keyword = component.get("v.keyWord");
+                        var action = component.get("c.searchProduct");
+
+                           action.setCallback(this, function(response) {
+                               if (response.getState() == "SUCCESS") {
+                                  var value = response.getReturnValue();
+                                  component.set("v.productList", value);
+                               }
+                               else{
+                                   console.log('error'+response.message);
+                               }
+                           });
+                           action.setParams({  keyWord : keyword  });
+                           $A.enqueueAction(action);
     }
 })
